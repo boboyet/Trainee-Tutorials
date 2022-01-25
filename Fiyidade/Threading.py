51e5d9fe3eb20eaabae74099a3e31877c5ab4ef0
Jessboy <fiyidads@icloud.com>
2022-01-25 10:27:07 +0100

    Threading examples

---
+++ Fiyidade/Threading.py
@@ -0,0 +1,31 @@
+import threading
+
+def function1():
+    #for x in range (100):
+        #print("OnE!")
+
+def function2():
+    #for x in range (1000):
+        #print("TwO!")
+   
+t1 = threading.Thread(target=function1)
+
+t2 = threading.Thread(target=function2)
+
+t1.start()
+t2.start()
+
+
+import threading
+
+def function1():
+    for x in range (50):
+        print ("Hello World")
+
+t1 = threading.Thread(target=function1)
+t1.start()
+
+t1.join()
+
+print("Another Transaction")
+
